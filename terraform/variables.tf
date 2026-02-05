83ffac6351ca0d485d6cf96ee38753c0b6036dbd
Arturo Gonzalez <arturo@arturosolutions.com.au>
2026-02-05 13:19:01 +0400

    Changed to Instance 002

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -60,13 +60,13 @@ variable "environment" {
 variable "instance_number" {
   description = "Instance number for resource naming (e.g., 001, 002)"
   type        = string
-  default     = "001"
+  default     = "002"
 }
 
 variable "bucket_suffix" {
   description = "Bucket suffix number (e.g., 001, 002)"
   type        = string
-  default     = "001"
+  default     = "002"
 }
 
 variable "resource_type" {
